7eff7215710994d97ebe4e418e6d62f28e3ca3ca
Mushraf Sultan <126558651+MushrafSultan@users.noreply.github.com>
2023-03-03 10:52:02 +0000

    Conversion

---
+++ 01_Basics/01-script.js
@@ -0,0 +1,33 @@
+
+// Numbers
+let score = undefined;
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumbers = Number (score)
+console.log(typeof (valueInNumbers))
+console.log(valueInNumbers)
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+// undefined => NaN
+
+// Boolean
+let isLoggedIn = "hitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 => ture
+// 0 => false
+// "" => false
+// "mushraf" => true
+
+// String
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
