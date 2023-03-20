14b3baf9adb7d00c508fbea02277723a1cc55356
Mushraf Sultan <126558651+MushrafSultan@users.noreply.github.com>
2023-03-20 05:17:00 +0000

    Numbers & Math and Javascript

---
+++ Learning_Javascript_Series/01-Variables.js
@@ -0,0 +1,22 @@
+
+
+
+// Let's learn Variables keywords and Data types
+
+// Creating Variables :
+const uid = "abc155";
+console.log(uid);
+
+const fullName = "Hitesh choudray"
+const email = "Hiteshchoudray@gmail.com"
+const password = 123456
+const confirmPassword = 123456
+const couresCount = 3500
+const isLoggedInFromGoogle = false
+
+
+// Take input from user
+// email = prompt("hello user give me your email address")
+
+console.log()
+prompt("Enter Your Name : " )
